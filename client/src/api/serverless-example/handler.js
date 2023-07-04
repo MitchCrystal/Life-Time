7ee04eadc731cbda8b2ec509eda9ed90b699883c
Chris Mitchell <christoph.mitchell@gmail.com>
2023-07-04 12:30:30 +0100

    feat: serverless function tweak

--- client/src/api/serverless-example/handler.js
+++ client/src/api/serverless-example/handler.js
@@ -1,5 +1,7 @@
 export default function handler(request, response) {
   response.status(200).json({
-    body: 'hello this is a response from the serverless function'
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
   });
 }
